107db6baf69cbd6dc5b47599af8520d61c1b6851
CDlihang <1332675356@qq.com>
2018-06-09 23:05:02 +0800

    updated

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -10,12 +10,17 @@
  */
 package com.controller;
 
+import com.entity.Type;
 import com.entity.User;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 /**
  * 〈一句话功能简述〉<br> 
@@ -31,10 +36,35 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/getUser")
-    public void getUser(){
+    @RequestMapping(value = "/getUser" , method = RequestMethod.GET)
+    public void getUser(@RequestParam("id") Integer id){
+        System.out.println(id);
+
         User userById = userService.getUserById(1);
         System.out.println(userById);
+    }
+
+    @RequestMapping("/getUser1/{id}")
+    public String getUser1(@PathVariable("id") Integer id){
+        System.out.println(id);
+        return "user/getUser";
+    }
+
+    //返回json格式数据
+    @ResponseBody
+
+    //映射url路径
+    @RequestMapping("/setUser")
+
+    //@ModelAttribute 请求数据绑定
+    public Map<String, Object> setUser(@ModelAttribute User user){
+
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("k", "ss");
+        stringObjectHashMap.put("kd", "sss");
+        stringObjectHashMap.put("user", user);
+
+        return stringObjectHashMap;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/entity/Type.java
@@ -0,0 +1,20 @@
+package com.entity;
+
+public class Type {
+    private String type;
+
+    public Type(String type) {
+        this.type = type;
+    }
+
+    public Type() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
