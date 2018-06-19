dd20895c9ea6dd8245a4593f53b8e77e601ad79d
CDlihang <1332675356@qq.com>
2018-06-19 17:05:50 +0800

    update

--- src/main/java/com/config/Swagger2Config.java
+++ src/main/java/com/config/Swagger2Config.java
@@ -1,124 +1,33 @@
 package com.config;
-
-import com.fasterxml.classmate.TypeResolver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.async.DeferredResult;
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.schema.WildcardType;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.ApiKeyVehicle;
-import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static springfox.documentation.schema.AlternateTypeRules.newRule;
-
 @EnableSwagger2
-@ComponentScan
 @Configuration
+@ComponentScan(basePackages = "com.controller")
 public class Swagger2Config {
     @Bean
-    public Docket petApi() {
-
-        Set<String> protocol = new HashSet<>();
-        protocol.add("http");
-//        protocol.add("http");
-
-        Docket d = new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-            .build()
-            .pathMapping("/")
-            //.directModelSubstitute(LocalDate.class, String.class)
-            .genericModelSubstitutes(ResponseEntity.class)
-            .alternateTypeRules(
-                newRule(typeResolver.resolve(DeferredResult.class,
-                    typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
-                    typeResolver.resolve(WildcardType.class)))
-            .useDefaultResponseMessages(false)
-            .globalResponseMessage(RequestMethod.GET,
-                newArrayList(new ResponseMessageBuilder()
-                    .code(500)
-                    .message("500 message")
-                    .responseModel(new ModelRef("Error"))
-                    .build()))
-            .securitySchemes(newArrayList(apiKey()))
-            .securityContexts(newArrayList(securityContext()))
-            .enableUrlTemplating(true)
-//            .globalOperationParameters(
-//                newArrayList(
-//                )
-//            )
-            .host("localhost:8080").protocols(protocol)
-            ;
-        return d;
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
     }
-
-    @Autowired
-    private TypeResolver typeResolver;
-
-    private ApiKey apiKey() {
-        return new ApiKey("mykey", "api_key", "header");
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-            .securityReferences(defaultAuth())
-            .forPaths(PathSelectors.regex("/anyPath.*"))
-            .build();
-    }
-
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-            = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return newArrayList(
-            new SecurityReference("mykey", authorizationScopes));
-    }
-
-    @Bean
-    SecurityConfiguration security() {
-        return new SecurityConfiguration(
-            "test-app-client-id",
-            "test-app-client-secret",
-            "test-app-realm",
-            "test-app",
-            "apiKey",
-            ApiKeyVehicle.HEADER,
-            "api_key",
-            "," /*scope separator*/);
-    }
-
-    @Bean
-    UiConfiguration uiConfig() {
-        return new UiConfiguration(
-            "validatorUrl",// url
-            "none",       // docExpansion          => none | list
-            "alpha",      // apiSorter             => alpha
-            "schema",     // defaultModelRendering => schema
-            UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS,
-            true,        // enableJsonEditor      => true | false
-            true,         // showRequestHeaders    => true | false
-            60000L);      // requestTimeout => in milliseconds, defaults to null (uses jquery xh timeout)
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("xxx项目接口文档")
+                .description("xxx项目接口测试")
+                .version("1.0.0")
+                .termsOfServiceUrl("")
+                .license("")
+                .licenseUrl("")
+                .build();
     }
 }
